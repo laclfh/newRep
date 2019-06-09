49a09d7d46d44f8dd147b778246e0b63e2b76bfe
laclfh <1271537629@qq.com>
2019-06-09 18:27:54 +0800

    提交说明

---
+++ resources/views/post/carousel.blade.php
@@ -0,0 +1,29 @@
+<div>
+    <div id="carousel-example" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example" data-slide-to="1"></li>
+            <li data-target="#carousel-example" data-slide-to="2"></li>
+        </ol><!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="/storage/banner1.jpg" alt="..." style="width: 617px;height: 266px;" />
+                <div class="carousel-caption">...</div>
+            </div>
+            <div class="item">
+                <img src="/storage/banner2.jpg" alt="..." style="width: 617px;height: 266px;" />
+                <div class="carousel-caption">...</div>
+            </div>
+            <div class="item">
+                <img src="/storage/banner3.jpg" alt="..." style="width: 617px;height: 266px;" />
+                <div class="carousel-caption">...</div>
+            </div>
+        </div>
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span></a>
+        <a class="right carousel-control" href="#carousel-example" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span></a>
+    </div>
+</div>
